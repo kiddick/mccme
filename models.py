0c0da845b1706bef1fe3af5db60e0a5306fac889
kiddick <rustdoc@yandex.ru>
2015-05-22 01:38:51 +0300

    usernames

--- models.py
+++ models.py
@@ -9,6 +9,7 @@ class Problem(models.Model):
 
 class UserProfile(models.Model):
     uid = models.IntegerField(default=0)
+    uname = models.CharField(max_length=200)
     solved_problems = models.ManyToManyField(Problem, related_name='solved_problems')
     unsolved_problems = models.ManyToManyField(Problem, related_name='unsolved_problems')
     tcount = models.IntegerField(default=0)
--- templatetags/user_filter.py
+++ templatetags/user_filter.py
@@ -5,5 +5,7 @@ register = template.Library()
 def _get(value, arg):
     if arg == 'uid':
         return value.uid
+    if arg == 'uname':
+        return value.uname
 
 register.filter('_get', _get)
