960c0d468fb1d16c462db62fc5b50236fd097315
kiddick <rustdoc@yandex.ru>
2015-05-21 16:36:16 +0300

    qm in url

--- urls.py
+++ urls.py
@@ -9,7 +9,8 @@ urlpatterns = patterns(
     url(r'^show_me/', views.show_me),
     url(r'^user/(?P<uid>\d+)/$', views.user_stats),
     url(r'^uid/(?P<uid>\d+)/$', views.show_user),
-    url('^uidx/(?P<uid>\d+)/?page=(?P<page>\d+)/$', views.multi_stats)
+    # url('^uidx/(?P<uid>\d+)/?page=(?P<page>\d+)/$', views.multi_stats)
+    url('^uidx/(?P<uid>\d+)/$', views.multi_stats)
 #   http://www.example.com/myapp/?page=3
 
 
