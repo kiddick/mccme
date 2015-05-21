cbbf659e96a428f859f3c048c38390f76a07da77
kiddick <rustdoc@yandex.ru>
2015-05-21 16:18:44 +0300

    multi

--- urls.py
+++ urls.py
@@ -8,6 +8,10 @@ urlpatterns = patterns(
     url(r'^action/', views.action),
     url(r'^show_me/', views.show_me),
     url(r'^user/(?P<uid>\d+)/$', views.user_stats),
-    url(r'^uid/(?P<uid>\d+)/$', views.show_user)
+    url(r'^uid/(?P<uid>\d+)/$', views.show_user),
+    url('^uidx/(?P<uid>\d+)/?page=(?P<page>\d+)/$', views.multi_stats)
+#   http://www.example.com/myapp/?page=3
+
+
 
 )
